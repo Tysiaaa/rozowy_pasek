7cc95e89e6b98141cadfef195fa43ca072735258
pwit <pwit@pwit.info>
2015-03-07 17:49:51 +0100

    Dopisany kod testujący funkcję suma.

--- rozowy_pasek.cpp
+++ rozowy_pasek.cpp
@@ -15,8 +15,47 @@ void suma(int t[], int n)
 
 }
 
+
+
+/*
+testOfSuma(void)
+Input: none
+Output: true if and only if suma(t,n) is correct (on some data) 
+*/
+bool testOfSuma(void) {
+
+	bool result;
+	// First test
+
+	int testInput1[] = { 0, 1, 2, 3, 4 };
+	int n = 5; // sizeof(testInput1);
+	int testOutput1[] = {0, 1, 3, 6, 10};
+
+	suma(testInput1, n);
+    
+	result = true;
+
+	for (int i = 0; i < n; i++)
+		if (testInput1[i] != testOutput1[i])
+			result = false;
+
+	// Second test
+
+	// ...
+
+	// Third test
+
+	// ...
+
+	return result;
+}
+
+
 int main()
 {
+
+	cout << "Function suma(t,n) is "<< (testOfSuma()?"correct.":"incorrect.");
+/*
 	int n;
 
 	cin >> n;
@@ -30,7 +69,7 @@ int main()
 	suma(t, n);
 
 	for (int i = 0; i < n; i++)
-		cout << t[i] << " ";
+		cout << t[i] << " ";*/
 
 	return 0;
 }
