f7486706b6525f5509ed437c972550a35e3648b3
Tysia <tysia.92@gmail.com>
2015-03-08 16:52:44 +0100

    na razie nic nie doda�am :panda_face:

--- rozowy_pasek.cpp
+++ rozowy_pasek.cpp
@@ -13,9 +13,45 @@ void suma(int t[], int n)
 }
 
 
+/*
+testOfSuma(void)
+Input: none
+Output: true if and only if suma(t,n) is correct (on some data)
+*/
+bool testOfSuma(void) {
+
+	bool result;
+	
+	// First test
+	int testInput1[] = { 0, 1, 2, 3, 4 };
+	int n = 5; // sizeof(testInput1);
+	int testOutput1[] = { 0, 1, 3, 6, 10 };
+
+	suma(testInput1, n);
+
+	result = true;
+
+	for (int i = 0; i < n; i++)
+		if (testInput1[i] != testOutput1[i])
+			result = false;
+
+	// Second test
+
+	// ...
+
+	// Third test
+
+	// ...
+
+	return result;
+}
+
+
 int main()
 {
-
+	cout << "Function suma(t,n) is " << (testOfSuma() ? "correct." : "incorrect.");
+	
+	/*
 	int n;
 
 	cin >> n;
@@ -29,7 +65,7 @@ int main()
 	suma(t, n);
 
 	for (int i = 0; i < n; i++)
-		cout << t[i] << " ";
+		cout << t[i] << " "; */
 
 	return 0;
 }
